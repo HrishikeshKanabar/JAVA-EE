669a04e54d25a8a4f235af29f6a947c63b097ecb
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2022-03-30 18:58:33 -0400

    Servlet context and config

--- WebDoctorAppV123/src/main/java/servlets/Login.java
+++ WebDoctorAppV123/src/main/java/servlets/Login.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// Step:1 import servlet config to get name of servlet and servlet context to get context parameter
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 /**
  * Servlet implementation class Register
  */
@@ -27,6 +31,26 @@ public class Login extends HttpServlet {
 		
 		System.out.println("EMAIL IS: "+ email + " PASWORD IS: "+ pass);
 		
+		// Step-2 display name of servlet with servlet config and servlet context parameter
+		
+		// // Creating object from class "ServletConfig"
+		
+		ServletConfig object = getServletConfig();
+		
+		String servletName = object.getServletName();
+		
+		System.out.println(servletName + " has been called");
+		
+		// // Creating object from class "ServletContext"
+		
+		ServletContext objectContext = getServletContext();
+		
+		String author =  objectContext.getInitParameter("Author");
+		String version=  objectContext.getInitParameter("Version");
+		String appName = objectContext.getInitParameter("AppName");
+	
+		
+		System.out.println("The app name is : " + appName + " "+ author + "@"+version);
 	
 	}
 
--- WebDoctorAppV123/src/main/java/servlets/Register.java
+++ WebDoctorAppV123/src/main/java/servlets/Register.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// Step-1 import servlet config and Servlet context
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 /**
  * Servlet implementation class Register
  */
@@ -27,5 +31,26 @@ public class Register extends HttpServlet {
 		System.out.println("fname is : "+ fname + " lname is : "
 		                    + lname + " email is :"
 		                    + email + " Password is: " + pass);
+		
+		// Step-2 Display name of Servlet
+		
+		// Creating object from class "ServletConfig"
+		
+		ServletConfig obj = getServletConfig();  
+		
+		String servName = obj.getServletName();
+		
+		System.out.println(servName + " has been called");
+		
+		// Create object from class "ServletContext"
+		
+		ServletContext objectContext = getServletContext();
+		
+		String author =  objectContext.getInitParameter("Author");
+		String version=  objectContext.getInitParameter("Version");
+		String appName = objectContext.getInitParameter("AppName");
+		
+		System.out.println("The app name is : " + appName + " "+ author + "@"+version);
+		
 	}
 }
